4d686ddb3ea0120094ef90c9627b259acf6faf21
Luke Waninger <luke.waninger@gmail.com>
2017-01-27 13:56:48 -0800

    .

--- NeuralNet/NeuralNet/NeuralNet.py
+++ NeuralNet/NeuralNet/NeuralNet.py
@@ -15,7 +15,7 @@ class NeuralNet(object):
         self.sizes     = sizes
         self.L         = len(sizes)
         self.iw_bounds = iw_bounds
-        self.w = []
+        self.w = [(0)]
         self.initialize_weights()
 
     def initialize_weights(self):
@@ -42,30 +42,33 @@ class NeuralNet(object):
     def forward_propagate(self, xi):
         """ moves data through layers of the network """
         z, a = [0], [xi] # started z with 0 to line up the indices with a[]
-        for wl in self.w:
-            a[-1] = np.insert(a[-1], 0, 1)
-            zv = a[-1].dot(wl)  # returns a (21,)
-            # should I reshape that to a (21,1) before appending it? yes
-            z.append(zv)
-            a.append(self.sigmoid(zv)) # do you activate the bias?
 
+        # for each hidden layer, apply the weights (with bias as w[0] and activate
+        for wl, l in zip(self.w[1:], range(self.L)):
+            a[l] = self.reshape(np.insert(a[l], 0, 1))  # insert a one for the bias
+            zv = a[l].T.dot(wl)                 # returns a (hidden-layer-size + 1,) vector
+            z.append(zv)                      
+
+            hl = self.sigmoid(zv)              # apply the activation function
+            a.append(hl)
         return z, a
 
-    def output_error(self, a3, z3, t):
-        return np.multiply((t - a3), sigmoid_prime(z3))
-
-    def back_propagate(self, h, t):
+    def back_propagate(self, a, t, z):
         """ compute the derivative of the cost functions to determine
         which way our weights should change for descent """
-        # first compute the partial derivatives for the second weight 
-        # matrix (hidden_layer_size X 1)... by chain rule
-        # shows back propagation error that each weight contributed from
-        # the hidden to output layers
-        s = [ok * (1 - ok) * (tk - ok) for tk, ok in zip(t, h[-1])]
-        for j, k in zip(range(self.L - 1, 1, 1), range(0, self.L - 1, 1)):
-            sj = sigmoid_prime(h[j]) * np.sum(self.w[j+1] * s[k]) 
-            s.append(sj)
-        return s.sort(reverse = True)
+        # 1. calculate the output error
+        s     = [0 for i in range(self.L)]
+        s[-1] = ((a[-1] - t.T) * self.sigmoid_prime(z[-1])).T
+
+        # reshape each z vector to account for the bias
+        #for l in range(1, self.L, 1):
+        #    z[l] = self.reshape(np.insert(z[l], 0, 1))
+
+        # 2. calculate the error for each hidden layer
+        for l in range(self.L - 2, -1, -1):
+            # TODO: VERIFY MATRIX SIZES ARE CORRECT AS THIS FUNCTION PASSES BACKWARDS
+            s[l] = np.multiply((self.w[l + 1][1:].dot(s[l + 1])), self.sigmoid_prime(z[l]))
+        return s
 
     def shuffle(self, X, y):
         """ shuffle the training data
@@ -75,6 +78,9 @@ class NeuralNet(object):
         """
         r = np.random.permutation(len(y))
         return X[r], y[r]
+
+    def reshape(self, m):
+        return m.reshape([m.shape[0], 1])
 
     def learn(self, X, y, eta, alpha):
         self.loss, train_acc, test_acc = [1], [], []
@@ -91,7 +97,7 @@ class NeuralNet(object):
                 # find the target and adjust tk and ok vector
                 # according to the homework assignment
                 o = np.argmax(a[-1])
-                t = np.array([.1 for i in range(0, 9)])
+                t = self.reshape(np.array([.1 for i in range(self.sizes[-1])]))
                 if o == tar[0]:
                     t[o] = .9
                 
@@ -99,7 +105,7 @@ class NeuralNet(object):
                 e_loss.append(0.5 * np.sum([(tk - ok)**2 for tk, ok in zip(t, a[-1])]))
 
                 # back-propagate
-                dw = self.back_propagate(a, t)
+                dw = self.back_propagate(a, t, z)
                 self.update_weights(dw)
 
         return self
