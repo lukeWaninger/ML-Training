ec6a3b2b9a9e9fe3304516af3fedf6545354ace6
LukeW <luke.waninger@gmail.com>
2017-02-08 23:44:13 -0800

    .

--- SupportVectorMachine/SupportVectorMachine/Main.py
+++ SupportVectorMachine/SupportVectorMachine/Main.py
@@ -1,5 +1,6 @@
 from sklearn.cross_validation   import train_test_split
 from sklearn.preprocessing      import StandardScaler
+from sklearn.preprocessing      import MinMaxScaler
 from sklearn.svm                import LinearSVC
 import matplotlib.pyplot        as plt
 import sklearn.metrics          as metrics
@@ -16,7 +17,7 @@ def main():
     # standardize everything
     stdsc   = StandardScaler()
     X_train = stdsc.fit_transform(X_train)
-    X_test  = stdsc.fit_transform(X_test)
+    X_test  = stdsc.transform(X_test)
 
     # show class distribution
     #print(collections.Counter(X_train))
@@ -42,8 +43,8 @@ def exp_one(X_train, y_train, X_test, y_test, filename):
     f.close()
 
     # get false/true positive rates and threshold from roc_curve
-    y_pred = svm.decision_function(X_test)
-    fpr, tpr, threshholds = metrics.roc_curve(y_true  = y_test, y_score = y_pred)
+    y_pred_dec = svm.decision_function(X_test)
+    fpr, tpr, threshholds = metrics.roc_curve(y_true  = y_test, y_score = y_pred_dec)
     roc_auc = metrics.auc(x = fpr, y = tpr)
 
     # plot the ROC
@@ -51,7 +52,7 @@ def exp_one(X_train, y_train, X_test, y_test, filename):
     plt.plot(fpr, tpr,
              color     = 'blue',
              linestyle = 'dashed',
-             label     = 'Linear SVM (auc = %0.2f)' % (roc_auc))
+             label     = 'Linear SVM (AUC = %0.2f)' % (roc_auc))
     
     # setup the figure
     plt.grid()
@@ -74,20 +75,37 @@ def exp_one(X_train, y_train, X_test, y_test, filename):
 def exp_two(X_train, y_train, X_test, y_test, svm, filename):
     # sort the training set feature space based on ||w_i||
     wm = svm.coef_
-    rank = np.argsort(svm.coef_)
+    rank = np.argsort(wm)[0][::-1]
     for i in range(X_train.shape[0]):
-        X_train[i] = np.array([X_train[i][j] for j in rank[0]])
+        X_train[i] = np.array([X_train[i][j] for j in rank])
     for i in range(X_test.shape[0]):
-        X_test[i] = np.array([X_test[i][j] for j in rank[0]])
-    
-    # print the ranking of feature importance
-    f = open(filename + '.txt', 'w')
-    [f.write('%d, ' % rank[0][i]) for i in range(rank[0].shape[0])]
-    f.close()
+        X_test[i] = np.array([X_test[i][j] for j in rank])
+
+    # show principle components (not part of assignment)
+    cov_mat = np.cov(X_train.T)
+    eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)    
+
+    #plot the variance explained ratio to show how the first dimensions
+    #account for the variance of the matrix
+    tot = sum(eigen_vals)
+    var_exp = [(i / tot) for i in sorted(eigen_vals, reverse = True)]
+    cum_var_exp = np.cumsum(var_exp)
+
+    #sort the eigenvectors based on their magnitudes
+    eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i], i) for i in range(len(eigen_vals))]
+    eigen_pairs.sort(reverse=True)
+
+    #next collect the eigenvectors that correspond to the two
+    #largest values (60% of the variance). The number of chosen
+    #vectors will be a trade off between computational efficiency
+    #and classifier performance
+    w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+                   eigen_pairs[1][1][:, np.newaxis]))
+
     
     acc = []
     # train and record accuracies for models including i features
-    for i in range(1, rank.shape[1] - 1, 1):
+    for i in range(1, rank.shape[0] - 1, 1):
         svc = LinearSVC()
         X_train_compressed = X_train[:,:i]
         X_test_compressed  = X_test[:,:i]
@@ -97,19 +115,43 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
         y_pred = svc.predict(X_test_compressed)
         acc.append(metrics.accuracy_score(y_test, y_pred))
     
-    # plot the accuracies
+    # setup plot
     plt.clf()
+    plt.grid()
+    plt.ylim((np.min(acc) - .02, np.max(acc) + .02))
+    plt.title('Accuracy per Features Used')
+    plt.xlabel('Features Included')
+    plt.ylabel('ROC Accuracy Score')
+    plt.legend(loc = 'best')
+
+    # plot the accuracy
     plt.plot([i for i in range(len(acc))], 
              acc,
              color     = 'blue',
              linestyle = 'solid',
-             label     = 'Accuracy')
-    
-    plt.grid()
-    plt.title('Accuracy per Features Used [ranked]')
-    plt.legend(loc = 'lower right')
-    plt.xlabel('Features Included')
-    plt.ylabel('ROC Accuracy Score')
+             linewidth = 3.0,
+             label     = 'Selected Features by ||w_i||')
+
+    # plot the variance by feature
+    min = np.min(acc)
+    var_exp_offset = [min for l in range(len(var_exp))]
+    plt.bar(range(1, len(var_exp) + 1), var_exp_offset, align = 'center', edgecolor = 'none', color = 'white')
+    plt.bar(range(1, len(var_exp) + 1), 
+            var_exp, 
+            alpha  = 0.5,
+            color  = 'lightgreen',
+            align  = 'edge',
+            bottom = var_exp_offset, 
+            label  = 'Adjusted Feature Variance')   
+   
+    # print some misc info
+    f = open(filename + '.txt', 'w')
+    [f.write('%d, ' % rank[k]) for k in range(rank.shape[0])]
+    f.write('\n\nEigen Pairs\n-------------\n%s\n' % [eigen_pairs[i][2] for i in range(0, len(eigen_pairs))])
+    f.write('\nEigenvalues\n-------------\n%s\n' % sorted(eigen_vals))
+    f.write('\nEigenvectors\n-------------\n%s\n' % eigen_vecs)
+    f.write('\nMatrix W:\n------------\n%s' % w)
+    f.close()
     
     # save data to file for later use
     plt.savefig(filename)
@@ -136,21 +178,22 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
         acc.append(metrics.accuracy_score(y_test, y_pred))
     
     # plot the data
-    plt.clf()
+    #plt.clf()
     plt.plot([i for i in range(len(acc))], 
              acc,
-             color     = 'blue',
+             color     = 'red',
+             linewidth = 3.0,
              linestyle = 'solid',
-             label     = 'Accuracy')
+             label     = 'Random Feature Selection')
     
-    plt.grid()
-    plt.title('Accuracy per Features by Feature [random]')
-    plt.legend(loc = 'lower right')    
-    plt.xlabel('Features Included')
-    plt.ylabel('ROC Accuracy Score')
+    #plt.grid()
+    #plt.title('Accuracy per Features by Feature [random]')
+    #plt.legend(loc = 'lower right')    
+    #plt.xlabel('Features Included')
+    #plt.ylabel('ROC Accuracy Score')
     
     # save to file
-    plt.savefig(filename)
+    plt.savefig(filename + '_all')
     pd.to_pickle((acc, order), filename)
 
 if __name__ == "__main__":
