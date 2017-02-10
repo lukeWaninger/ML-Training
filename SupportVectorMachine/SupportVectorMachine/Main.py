ebf7c3bbf212c228c6f0cd66952e021190b8f2fb
Luke Waninger <luke.waninger@gmail.com>
2017-02-09 17:32:13 -0800

    .

--- SupportVectorMachine/SupportVectorMachine/Main.py
+++ SupportVectorMachine/SupportVectorMachine/Main.py
@@ -12,7 +12,7 @@ def main():
     # read in the data
     data = pd.read_csv("spambase.data", header = None)
     X, y = data.iloc[:,:-1].values, data.iloc[:,-1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, random_state = 123)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5) # random_state = 123)
 
     # standardize everything
     stdsc   = StandardScaler()
@@ -28,15 +28,15 @@ def main():
 
 def exp_one(X_train, y_train, X_test, y_test, filename):
     # initialize and fit the SVM
-    svm = LinearSVC(random_state = 1)
+    svm = LinearSVC() #random_state = 1)
     svm.fit_transform(X_train, y_train)
 
     # predict with the test set
     y_pred = svm.predict(X_test)
 
     # output accuracy, precision, and recall to text file
-    f = open(filename + '.txt', 'w')
-    f.write('Accuracy Score: %.3f; Precision: %.3f; Recall %.3f' %
+    f = open(filename + '.txt', 'a')
+    f.write('Accuracy Score: %.3f; Precision: %.3f; Recall %.3f\n' %
           (metrics.accuracy_score(y_test, y_pred),
            metrics.precision_score(y_test, y_pred),
            metrics.recall_score(y_test, y_pred)))
@@ -77,12 +77,9 @@ def exp_one(X_train, y_train, X_test, y_test, filename):
     
 def exp_two(X_train, y_train, X_test, y_test, svm, filename):
     # sort the training set feature space based on ||w_i||
-    wm = svm.coef_
-    rank = np.argsort(wm)[0][::-1]
-    for i in range(X_train.shape[0]):
-        X_train[i] = np.array([X_train[i][j] for j in rank])
-    for i in range(X_test.shape[0]):
-        X_test[i] = np.array([X_test[i][j] for j in rank])
+    rank = np.argsort(svm.coef_)[0][::-1]
+    X_train_sorted = X_train[:, rank]
+    X_test_sorted  = X_test[:, rank]
 
     # show principle components (not part of assignment)
     cov_mat = np.cov(X_train.T)
@@ -111,8 +108,8 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
     # train and record accuracies for models including i features
     for i in range(1, rank.shape[0] - 1, 1):
         svc = LinearSVC()
-        X_train_compressed = X_train[:,:i]
-        X_test_compressed  = X_test[:,:i]
+        X_train_compressed = X_train_sorted[:,:i]
+        X_test_compressed  = X_test_sorted[:,:i]
         svc.fit_transform(X_train_compressed, y_train)
 
         # predict with the test set
@@ -122,24 +119,24 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
     # setup plot
     plt.clf()
     plt.grid()
-    plt.ylim((np.min(acc), np.max(acc) + .02))
+    plt.ylim((np.min(acc), .95))
     plt.xlim((0, 57))
     plt.title('Accuracy per Features Used')
     plt.xlabel('Features Included')
     plt.ylabel('ROC Accuracy Score')
 
     # plot the accuracy
-    plt.plot([i for i in range(len(acc))], 
+    plt.plot([i + 1 for i in range(len(acc))], 
              acc,
              color     = 'blue',
              linestyle = 'solid',
              linewidth = 3.0,
              label     = 'Accuracy')
 
-    # plot the variance by feature
+    # plot the feature variance
     min = np.min(acc)
     var_exp_offset = [min for l in range(len(var_exp))]
-    plt.bar(range(1, len(var_exp) + 1), var_exp_offset, align = 'center', edgecolor = 'none', color = 'white')
+    plt.bar(range(0, len(var_exp)), var_exp_offset, align = 'center', edgecolor = 'none', color = 'white')
     plt.bar(range(0, len(var_exp)), 
             var_exp, 
             alpha  = 0.3,
@@ -147,7 +144,7 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
             edgecolor = 'blue',
             align  = 'edge',
             bottom = var_exp_offset, 
-            label  = 'Feature Variance')       
+            label  = 'Feature Covariance (ranked)')       
     plt.legend(loc = 'upper left',
                ncol     = 1, 
                fontsize = 'small',
@@ -155,14 +152,17 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
                shadow   = True)
 
     # print some misc info
-    f = open(filename + '.txt', 'w')
-    [f.write('%d, ' % rank[k]) for k in range(rank.shape[0])]
+    f = open(filename + '.txt', 'a')
+    f.write('\n')
+    [f.write('%d, ' % rank[k]) for k in range(rank.shape[0])]    
+    """
     #f.write('\n\nEigen Pairs\n-------------\n%s\n' % [eigen_pairs[i][2] for i in range(0, len(eigen_pairs))])
     #f.write('\nEigenvalues\n-------------\n%s\n' % sorted(eigen_vals))
     #f.write('\nEigenvectors\n-------------\n%s\n' % eigen_vecs)
     #f.write('\nMatrix W:\n------------\n%s' % w)
     f.close()
-    
+    """
+
     # save data to file for later use
     plt.savefig(filename)
     pd.to_pickle((acc, rank), filename)
@@ -170,11 +170,9 @@ def exp_two(X_train, y_train, X_test, y_test, svm, filename):
 def exp_three(X_train, y_train, X_test, y_test, filename):
     # generate random order of features
     order = np.random.permutation(len(X_test[0]))
-    for i in range(X_train.shape[0]):
-        X_train[i] = np.array([X_train[i][j] for j in order])
-    for i in range(X_test.shape[0]):
-        X_test[i] = np.array([X_test[i][j] for j in order])
-    
+    X_train_rand = X_train[:, order]
+    X_test_rand  = X_test[:, order]
+
     acc = []
     # train models including the i number of random features
     for i in range(1, len(order) - 1, 1):
@@ -188,13 +186,15 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
         acc.append(metrics.accuracy_score(y_test, y_pred))
     
     # plot the data
-    plt.plot([i for i in range(len(acc))], 
+    plt.ylim((np.min(acc), .95))
+    plt.plot([i + 1 for i in range(len(acc))], 
              acc,
              color     = 'red',
              linewidth = 3.0,
              linestyle = 'solid',
              label     = 'Random Selection')        
 
+    # calculate feature covariance
     cov_mat = np.cov(X_train.T)
     eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)    
 
@@ -204,7 +204,7 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
     var_exp = [(i / tot) for i in eigen_vals]
     var_exp_orderd = [var_exp[k] for k in range(len(var_exp))]
     var_exp_offset = [np.min(acc) for l in range(len(var_exp))]
-    plt.bar(range(1, len(var_exp) + 1), var_exp_offset, align = 'center', edgecolor = 'none', color = 'white')
+    plt.bar(range(0, len(var_exp)), var_exp_offset, align = 'center', edgecolor = 'none', color = 'white')
     plt.bar(range(0, len(var_exp)), 
             var_exp_orderd, 
             alpha  = 0.3,
@@ -212,18 +212,19 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
             edgecolor = 'red',
             align  = 'edge',
             bottom = var_exp_offset, 
-            label  = 'Feature Variance (random order)')    
-    plt.legend(loc = 'upper right',
+            label  = 'Feature Covariance (random)')    
+    plt.legend(loc = 'upper left',
                ncol = 2, 
                fancybox = True, 
                fontsize = 'small',
                shadow = True)
     plt.savefig(filename + '_all')
 
+    # plot experiment 3 data by itself
     plt.clf()
     plt.ylim((np.min(acc), np.max(acc) + .02))
     plt.xlim((0, 57))
-    plt.plot([i for i in range(len(acc))], 
+    plt.plot([i + 1 for i in range(len(acc))], 
              acc,
              color     = 'red',
              linewidth = 3.0,
@@ -238,7 +239,7 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
             edgecolor = 'red',
             align  = 'edge',
             bottom = var_exp_offset, 
-            label  = 'Feature Variance')
+            label  = 'Feature Covariance')
     plt.legend(loc = 'upper left',
                ncol = 2, 
                fancybox = True, 
@@ -251,11 +252,9 @@ def exp_three(X_train, y_train, X_test, y_test, filename):
             fontsize = 'small', 
             fancybox = True, 
             shadow = True) 
-    plt.savefig(filename + '_all')
     plt.grid()
-
-    # save to file
     plt.savefig(filename)
+
     pd.to_pickle((acc, order), filename)
 
 if __name__ == "__main__":
